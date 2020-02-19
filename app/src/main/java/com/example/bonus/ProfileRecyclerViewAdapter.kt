01f3b407cf21e692d2f65ab5f8761e9eb4e7a7a9
John Barbosa <djbarbosa@uninorte.edu.co>
2020-02-19 01:28:14 -0500

    Recycler view data binding

--- app/src/main/java/com/example/bonus/ProfileRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/bonus/ProfileRecyclerViewAdapter.kt
@@ -5,7 +5,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.bonus.databinding.FragmentItemBinding
 import com.example.classvideos.models.ProfileModel
 import kotlinx.android.synthetic.main.fragment_item.view.*
 
@@ -17,37 +19,43 @@ class ProfileRecyclerViewAdapter(
         parent: ViewGroup,
         viewType: Int
     ): ProfileRecyclerViewAdapter.ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_item, parent, false)
-        return ViewHolder(view)
+        //val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_item, parent, false)
+        //return ViewHolder(view)
+        var binder: FragmentItemBinding
+        binder = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.fragment_item, parent, false)
+        return ViewHolder(binder)
     }
 
     override fun getItemCount(): Int = mValues.size
 
     override fun onBindViewHolder(holder: ProfileRecyclerViewAdapter.ViewHolder, position: Int) {
         val item = mValues[position]
-        holder.tFirstName.text = item.first_name
-        holder.tLastName.text = item.last_name
+        //holder.tFirstName.text = item.first_name
+        //holder.tLastName.text = item.last_name
 
-        holder.mView.setOnClickListener({
+        holder.mView.card.setOnClickListener({
             mListener?.onListItemInteraction(item)
         })
 
         //holder.button.setOnClickListener({
         //    mListener?.onListButtonInteraction(item)
         //})
+
+        holder.mView.user = item
+        holder.mView.executePendingBindings()
     }
 
     //public fun updateData() {
     //    notifyDataSetChanged()
     //}
 
-    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+    inner class ViewHolder(val mView: FragmentItemBinding) : RecyclerView.ViewHolder(mView.root) {
         //val button: Button = mView.bDeleteUser
-        val tFirstName: TextView = mView.first_name
-        val tLastName: TextView = mView.last_name
+        //val tFirstName: TextView = mView.first_name
+        //val tLastName: TextView = mView.last_name
     }
 
-    interface onListInteraction {
+    public interface onListInteraction {
         fun onListItemInteraction(item: ProfileModel?)
     //    fun onListButtonInteraction(item: ProfileModel?)
     }
