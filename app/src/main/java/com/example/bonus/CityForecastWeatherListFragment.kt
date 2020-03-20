6eddacea21ea79ddb4c443d69e41e08def036045
John Barbosa <djbarbosa@uninorte.edu.co>
2020-03-20 00:49:56 -0500

    Filtering the weather forecast of the next days for the current time. I. It seems to be working.

--- app/src/main/java/com/example/bonus/CityForecastWeatherListFragment.kt
+++ app/src/main/java/com/example/bonus/CityForecastWeatherListFragment.kt
@@ -2,11 +2,11 @@ package com.example.bonus
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
@@ -17,6 +17,10 @@ import com.example.bonus.models.CityCurrentWeatherModel
 import com.example.bonus.models.CityForecastWeatherModel
 import com.example.bonus.models.CityForecastWeatherViewModel
 import kotlinx.android.synthetic.main.fragment_city_forecast_weather_list.view.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
 
 /**
  * A simple [Fragment] subclass.
@@ -43,8 +47,35 @@ class CityForecastWeatherListFragment : Fragment(), CityForecastWeatherRecyclerV
             run {
                 Log.d("PRVA",  "3")
                 cityForecastWeatherModel.clear()
+                //if (elementDateTime.dayOfMonth.toInt() - currentDateTime.dayOfMonth.toInt() == 0) {
+
+                //}
+                var currentDateTime = LocalDateTime.parse(
+                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("y-M-d HH:mm:ss")),
+                    DateTimeFormatter.ofPattern("y-M-d HH:mm:ss")
+                )
                 for (element in cityForecast) {
-                    cityForecastWeatherModel.add(element)
+                    val elementDateTime = LocalDateTime.parse(
+                        element.dt_txt,
+                        DateTimeFormatter.ofPattern("y-M-d HH:mm:ss")
+                    )
+
+                    Log.d("PRVA",  "${currentDateTime.dayOfMonth} VS ${elementDateTime.dayOfMonth} | ${currentDateTime.hour} VS ${elementDateTime.hour}")
+
+                    if (elementDateTime.dayOfMonth.toInt() - currentDateTime.dayOfMonth.toInt() == 0 || currentDateTime.hour == 0) {
+                        Log.d("PRVA",  "true") //${elementDateTime.dayOfMonth.toInt() - currentDateTime.dayOfMonth.toInt()}")
+                        val hourDifference = currentDateTime.hour - elementDateTime.hour
+                        if (hourDifference in 0..3) {
+                            Log.d("PRVA",  "true ${elementDateTime.hour - currentDateTime.hour}")
+                            currentDateTime = currentDateTime.plusDays(1)
+                            cityForecastWeatherModel.add(element)
+                        } else {
+                            Log.d("PRVA",  "false ${currentDateTime.hour - elementDateTime.hour}")
+                        }
+                    } else {
+                        Log.d("PRVA",  "false") // ${elementDateTime.dayOfMonth.toInt() - currentDateTime.dayOfMonth.toInt()}")
+                    }
+                    //Log.d("PRVA",  "$currentDateTime VS $elementDateTime / $days | $hours | $minutes")
                 }
                 Log.d("PRVA",  "citiesProfileModel.size = "+cityForecastWeatherModel.size)
                 adapter!!.notifyDataSetChanged()
